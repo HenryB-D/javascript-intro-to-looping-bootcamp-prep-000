133121613646cb7405aaaf596a13e5a65d10af26
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-05 13:02:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,3 +25,10 @@ function doWhileLoop(array){
   }while ([array.length>0 && incrementVariable()]);
 return array;
 }
+
+function doWhileLoop(array){
+do {
+  array.pop()
+} while (array.length>0 && maybeTrue())
+return array
+}
